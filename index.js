66916841f03369d1425d9d0d256682a0b0cd4810
Rodrigo Gomes <cklrod@hotmail.com>
2022-05-25 21:44:06 -0300

    regex, capturando links

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+function extrairLinks(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResultados = [];
+    let temp;
+    while((temp = regex.exec(texto)) !== null){
+        arrayResultados.push({[temp[1]]: temp[2]})
+    }
+
+    return arrayResultados;
+}
+
 function tratamentoErro(erro) {
     throw new Error(chalk.red(erro.code, 'Erro no caminho do diretório'));
 }
@@ -9,13 +20,13 @@ async function leitura(caminho) {
     try {
         const encoding = 'utf8';
         const texto = await fs.promises.readFile(caminho, encoding);
-        console.log(chalk.green(texto));
-    }catch(erro){
+        console.log(extrairLinks(texto));
+    } catch (erro) {
         tratamentoErro(erro);
-    }finally{
+    } finally {
         console.log(chalk.blue('Concluído!'));
     }
-    
+
 }
 
 leitura('./arquivos/texto1.md');
